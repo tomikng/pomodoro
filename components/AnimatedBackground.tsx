37463ef97436495c044a6c9073317d924348235f
Hai Hung Nguyen <tomasnguyen43@gmail.com>
2024-08-25 08:54:34 +0200

    fix: add bg

--- components/AnimatedBackground.tsx
+++ components/AnimatedBackground.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const AnimatedBackground: React.FC = () => {
   return (
     <div className="animated-background">
-      {[...Array(5)].map((_, index) => (
+      {[...Array(7)].map((_, index) => (
         <div key={index} className={`floating-circle circle-${index + 1}`} />
       ))}
       <style jsx>{`
@@ -16,7 +16,7 @@ const AnimatedBackground: React.FC = () => {
           right: 0;
           bottom: 0;
           overflow: hidden;
-          background: linear-gradient(45deg, #f3f4f6, #e5e7eb);
+          background: hsl(0, 0%, 97%);
           z-index: -1;
         }
         .floating-circle {
@@ -24,8 +24,9 @@ const AnimatedBackground: React.FC = () => {
           width: 150px;
           height: 150px;
           border-radius: 50%;
-          background-color: rgba(100, 100, 255, 0.3);
-          animation: float 20s infinite ease-in-out;
+          background-color: hsla(0, 0%, 100%, 0.5);
+          box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+          animation: float 30s infinite ease-in-out;
         }
         .circle-1 {
           top: 10%;
@@ -52,32 +53,30 @@ const AnimatedBackground: React.FC = () => {
           left: 50%;
           animation-delay: 10s;
         }
-
         .circle-6 {
-          top: 30%;
-          left: 10%;
-          animation-delay: 10s;
+          top: 60%;
+          left: 40%;
+          animation-delay: 12s;
         }
-
         .circle-7 {
           top: 70%;
           left: 80%;
-          animation-delay: 10s;
+          animation-delay: 15s;
         }
 
         @keyframes float {
           0%,
           100% {
-            transform: translate(0, 0) scale(1);
+            transform: translate(0, 0);
           }
           25% {
-            transform: translate(50px, 50px) scale(1.1);
+            transform: translate(20px, 20px);
           }
           50% {
-            transform: translate(100px, -50px) scale(0.9);
+            transform: translate(40px, -20px);
           }
           75% {
-            transform: translate(-50px, 100px) scale(1.2);
+            transform: translate(-20px, 40px);
           }
         }
       `}</style>
